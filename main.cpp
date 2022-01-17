d24ff59720ce1bd04001253cdd5112024bdb4fa2
Regan Willis <reganwillis@outlook.com>
2022-01-17 12:58:56 -0600

    function to randomly get image

--- main.cpp
+++ main.cpp
@@ -3,11 +3,41 @@
 
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+
+// get pixel art by randomly chosing one of 4 images
+sf::Image getImage() {
+    // choose random number
+    int numOfImageChosen = rand() % 1000;
+    std::cout << numOfImageChosen;  // debug
+
+    // declare image
+    sf::Image imageChosen;
+
+    // randomly load the image
+    if (numOfImageChosen <= 899)
+        imageChosen.loadFromFile("Images/little-flower-common.png");
+    else if (numOfImageChosen > 899 && numOfImageChosen <= 989)
+        imageChosen.loadFromFile("Images/little-flower-uncommon.png");
+    else if (numOfImageChosen > 989 && numOfImageChosen <= 998)
+        imageChosen.loadFromFile("Images/little-flower-rare.png");
+    else if (numOfImageChosen == 999)
+        imageChosen.loadFromFile("Images/little-flower-ultrarare.png");
+    else {
+        // default
+        std::cout << "ERROR: numOfImageChosen out of bounds: " << numOfImageChosen << ". Loading common image.";
+        imageChosen.loadFromFile("Images/little-flower-common.png");
+    }
+
+    return imageChosen;
+}
+
+// TODO: color palette by randomly chosing one of 4 color palettes
 
 // TODO: pass in color palette
 sf::Color changePixelColor(sf::Color currColor) {
@@ -29,15 +59,19 @@ sf::Color changePixelColor(sf::Color currColor) {
 
 int main()
 {
-    // TODO: randomly choose one of 5 images
-    // load in the image
-    sf::Image loadedImg;
-    loadedImg.loadFromFile("Images/little-flower.png");
+    // init random seed
+    srand(time(0));
+
+    // TESTING getImage FUNCTION
+    sf::Image img = getImage();
+    img.saveToFile("./Images/chosenImage.png");
 
     // TODO: randomly choose a color palette
     // TODO: if color palette chosen was basic -> do not change the image
     // TODO: else -> recreate the image with new color palette
-
+    
+    // TESTING changePixelColor FUNCTION
+    /*
     // create new image with same dimensions as old image
     sf::Image newImg;
     newImg.create(loadedImg.getSize().x, loadedImg.getSize().y, sf::Color::Black);
@@ -51,21 +85,6 @@ int main()
 
     // save new image to desktop
     newImg.saveToFile("./Images/newImg.png");
-
+    */
     return 0;
 }
-
-/*
-// TODO: function to randomly choose which pixel art to draw and what color
-float getRandom(float percentChosen) {
-    // random
-    //srand(static_cast<unsigned>(time(NULL)));
-    srand(static_cast<unsigned>(time(NULL)));
-
-    // random between two vars
-    //static_cast<float>(rand() % static_cast<int>(this->window->getSize().x - this->enemy.getSize().x)), 0.f;
-
-    // random between 0 and 4?
-    int type = rand() % 5;
-    return rand()%5 < (percentChosen * 5);
-}*/
